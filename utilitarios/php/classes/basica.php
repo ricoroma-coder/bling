eeb459110a9ea2efb5c5fe74d35c8bf6e9fca9ca
Henrique Roma <roma.fulldev@gmail.com>
2021-07-03 14:44:03 -0300

    Ajuste classe Basica

--- utilitarios/php/classes/basica.php
+++ utilitarios/php/classes/basica.php
@@ -7,8 +7,18 @@ class Basica
         return $this->$_atributo;
     }
 
-    public function atribuir($_atributo, $_valor)
+    public function atribuir($_atributo, $_valor, $_array = false, $_index = "")
     {
-        $this->$_atributo = $_valor;
+        if ($_array)
+        {
+            if (empty($_index))
+                $this->$_atributo[] = $_valor;
+            else
+                $this->$_atributo[$_index] = $_valor;
+        }
+        else
+        {
+            $this->$_atributo = $_valor;
+        }
     }
 }
